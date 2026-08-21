6de0322ddb114d7ff58eb91d7b9fe011bb708e83
AmbiyaTareen <ambiyatareen@gmail.com>
2026-08-21 18:27:45 +0530

    Add Titanic dataset and analysis scripts

--- titanic.py
+++ titanic.py
@@ -1,12 +1,45 @@
-<<<<<<< HEAD
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
-df = pd.read_csv("Titanic-Dataset.csv")
-print(df.groupby("Survived")["Pclass"].value_counts())
-=======
-import pandas as pd
-
-df = pd.read_csv("Titanic-Dataset.csv")
-print(df.groupby("Survived")["Pclass"].value_counts())
->>>>>>> 6dd9691552871b3996124ea738570b6a08594b31
+df =pd.read_csv('Titanic-Dataset.csv')
+print(df.shape)
+print(df.head)
 print(df.isnull().sum())
+
+df['Sex']=df['Sex'].map({'male': 0 , 'female': 1})
+print(df['Sex'].unique())
+print(df['Sex'].dtype)
+
+df['Age'] = df['Age'].fillna(df['Age'].median())
+print(df['Age'].isnull().sum())
+
+X= df[['Pclass', 'Sex', 'Age', 'Fare']]
+y = df['Survived']
+
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+
+model=RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy=accuracy_score(y_test,y_pred)
+print(accuracy)
+print(classification_report(y_test,y_pred))
+
+comparison = X_test.copy()
+comparison['Actual'] = y_test
+comparison['Predicted'] = y_pred
+comparison['Correct?'] = comparison['Actual'] == comparison['Predicted']
+print(comparison.head(10))
+
+
